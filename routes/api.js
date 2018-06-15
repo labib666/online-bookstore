8b8da189f558c5b01cd64e75ed1c1e1534b9de8a
Hasib <himuhasib@gmail.com>
2018-06-16 01:03:03 +0600

    Improve HTTP logger

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
+    res.status(200);
     res.send('Hello from API');
+    next();
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,25 @@ const web = require('./web');
 const api = require('./api');
 const chalk = require('chalk');
 
-const HttpNotFound = (req, res) => {
-    console.log(chalk.red.bold(req.method, req.originalUrl, ' - 404 Not found'));
-    res.send('<center><h1>404 Not found</h1></center>');
+const HttpNotFound = (req, res, next) => {
+    if(!res.headersSent) {
+        res.status(404);
+        res.send('<center><h1>404 Not found</h1></center>');
+    }
+    next();
 };
 
 const Logger = (req, res, next) => {
     const time = new Date();
-    console.log(chalk.bold(time.toISOString(), req.method), req.originalUrl);
+    console.log(chalk.bold(time.toISOString(), req.method, res.statusCode), req.originalUrl);
     next();
 };
 
 const router = express.Router();
-router.use(Logger);
+
 router.use('/api', api);
 router.use('/', web);
-router.all('*', HttpNotFound);
+router.use(HttpNotFound);
+router.use(Logger);
 
 module.exports = router;
