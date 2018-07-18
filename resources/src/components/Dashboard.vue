739997a610c7bc16f5320f4b3a0c384a8591fe12
Hasib <himuhasib@gmail.com>
2018-07-19 03:04:11 +0600

    Add topbar in Dashboard area

---
+++ resources/src/components/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Topbar></Topbar>
+    </div>
+</template>
+
+<script>
+import Topbar from './Topbar';
+export default {
+    components: {
+        Topbar
+    }
+}
+</script>
+
--- resources/src/views/Dashboard.vue
+++ resources/src/views/Dashboard.vue
@@ -2,18 +2,20 @@
     <div id="dashboard">
         <AttemptAuth v-if="!isAuthSuccess"></AttemptAuth>
         <div v-if="isAuthSuccess">
-            Dashboard area
+            <Dashboard></Dashboard>
         </div>
     </div>
 </template>
 
 <script>
 import AttemptAuth from '@/components/AttemptAuth';
+import Dashboard from '@/components/Dashboard';
 import { mapState } from 'vuex';
 
 export default {
     components: {
-        AttemptAuth
+        AttemptAuth,
+        Dashboard
     },
 
     computed: {
