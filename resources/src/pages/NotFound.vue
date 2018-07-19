3b57e785e0308b01ea85f50338435b3943ab6118
Hasib <himuhasib@gmail.com>
2018-07-19 22:12:24 +0600

    Create 404 page

---
+++ resources/src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <div class="flex-center full-height">
+            <div class="flex-container">
+            <h1>Oops... You are lost :(</h1>
+            <br />
+            <center><a href="/">Home</a></center>
+            </div>
+        </div>
+    </div>
+</template>
