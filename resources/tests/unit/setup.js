3c5fea1c9e384af1fddeb2491b23cc5eda368936
Hasib <himuhasib@gmail.com>
2018-07-27 08:46:10 +0600

    Add test for logout

--- resources/tests/unit/setup.js
+++ resources/tests/unit/setup.js
@@ -1,13 +1,16 @@
 import VueAxios from 'vue-axios';
 import axios from 'axios';
 import Notifications from 'vue-notification';
+import store from '@/store';
 import Router from 'vue-router';
+import Vuex from 'vuex';
 import { createLocalVue, mount } from '@vue/test-utils';
 
 export function setup (component) {
     const router = new Router({});
 
     const localVue = createLocalVue();
+    localVue.use(Vuex);
     localVue.use(VueAxios, axios);
     localVue.use(Notifications);
     localVue.use(Router);
@@ -16,7 +19,8 @@ export function setup (component) {
 
     return mount(component, {
         localVue,
-        router
+        router,
+        store
     });
 }
 
