7267719f7cf7188998cd2cd38cb8a8d1154908ec
Hasib <himuhasib@gmail.com>
2018-07-29 03:40:29 +0600

    Write tests for register

---
+++ resources/tests/unit/Register.spec.js
@@ -0,0 +1,62 @@
+import { expect } from 'chai';
+import Register from '@/views/Register';
+import { setup, cleanUp } from './setup';
+import { type, checkAtNthLoop, click } from '../utils';
+import sinon from 'sinon';
+
+describe('Register.vue', () => {
+    let wrapper, sandbox;
+
+    beforeEach(() => {
+        wrapper = setup(Register);
+        sandbox = sinon.createSandbox();
+    });
+
+    afterEach(() => {
+        cleanUp();
+        sandbox.restore();
+    });
+
+    it('has register button', () => {
+        expect(wrapper.contains('button')).to.equal(true);
+    });
+
+    it('requests to /register', (done) => {
+        let stub = sandbox.stub(wrapper.vm.$http, 'post').callsFake((path, data) => {
+            expect(path).to.equal('/register');
+            return Promise.resolve();
+        });
+
+        wrapper.vm.register();
+
+        checkAtNthLoop(() => {
+            sinon.assert.calledOnce(stub);
+            done();
+        });
+    });
+
+    it('sends the correct data', (done) => {
+        let stub = sandbox.stub(wrapper.vm.$http, 'post').callsFake((path, data) => {
+            expect(data).to.deep.equal({
+                username: 'user',
+                password: '123456',
+                email: 'test@example.com',
+                name: 'Name'
+            });
+
+            return Promise.resolve();
+        });
+
+        type(wrapper, '#username', 'user');
+        type(wrapper, '#password', '123456');
+        type(wrapper, '#email', 'test@example.com');
+        type(wrapper, '#name', 'Name');
+
+        click(wrapper, 'button');
+
+        checkAtNthLoop(() => {
+            sinon.assert.calledOnce(stub);
+            done();
+        });
+    });
+});
